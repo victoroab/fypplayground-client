57a225e1e11e256cc120db0cce22125292cc5240
victoroab <96608980+victoroab@users.noreply.github.com>
2023-05-23 18:52:19 +0100

    updated endpoint

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const Axios = axios.create({
-  baseURL: 'https://fyp-playground-server-1.herokuapp.com/',
+  baseURL: 'https://fyp-playground-server-1.herokuapp.com/api',
 })
