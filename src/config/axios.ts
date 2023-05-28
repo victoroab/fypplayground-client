2aecae38cfea980b5f20c4a68b8ecf65b82edbcc
victoroab <96608980+victoroab@users.noreply.github.com>
2023-05-28 17:18:47 +0100

    using heroku api

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+// export const Axios = axios.create({
+//   baseURL: 'http://localhost:3500/api',
+// })
+
 export const Axios = axios.create({
   baseURL: 'https://fyp-playground-server-1.herokuapp.com/api',
 })
