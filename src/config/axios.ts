7fb67305e6f6e2277d5c2b108f1f8cbb0d8d3a81
victoroab <96608980+victoroab@users.noreply.github.com>
2023-06-25 19:05:39 +0100

    Prod-test

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export const Axios = axios.create({
-  baseURL: 'http://localhost:3500/api',
-})
-
 // export const Axios = axios.create({
-//   baseURL: 'https://foregoing-thing-production.up.railway.app/api',
+//   baseURL: 'http://localhost:3500/api',
 // })
+
+export const Axios = axios.create({
+  baseURL: 'https://foregoing-thing-production.up.railway.app/api',
+})
