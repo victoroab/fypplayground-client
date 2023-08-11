04e96ece515149e13caf52942cd4e2a933c486d8
victoroab <96608980+victoroab@users.noreply.github.com>
2023-08-11 19:46:59 +0100

    fixed env import

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const URL = import.meta.env.SERVER_ENDPOINT as string
+const URL = import.meta.env.VITE_SERVER_ENDPOINT as string
 
 // export const Axios = axios.create({
 //   baseURL: 'http://localhost:3500/api',
